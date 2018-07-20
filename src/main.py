b399dd95eef3051e7506e000c3572d28aa1b3527
shuyangw <qwqw1337@gmail.com>
2018-07-19 21:02:45 -0400

    Added screen cap functionality and conversion of pixels to grayscale.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from deepq import init
-from screen import get_window_rect
+from screen import get_window_rect, get_window_pixels
 
 if __name__ == "__main__":
-    dim = get_window_rect()
+    rect = get_window_rect()
+    print(rect)
+    get_window_pixels(rect)
--- src/screen.py
+++ src/screen.py
@@ -1,8 +1,27 @@
 import win32gui
 import sys
+import time
+
+from PIL import ImageGrab
+
+def rgb_to_grayscale(pixel):
+	return pixel[0] * 0.299 + pixel[1] * 0.587 + pixel[2] * 0.114
+
+def get_window_pixels(rect):
+	count = 0
+	pixels = ImageGrab.grab().load()
+	y_range = rect[3] - rect[1]
+	x_range = rect[2] - rect[0]
+	export_pixels = [[0 for y in y_range] for x in x_range]
+	for y in range(rect[1], rect[3]):
+		for x in range(rect[0], rect[2]):
+			export_pixels[x,y] = rgb_to_grayscale(pixels[x,y])
+			# print(pixels[x,y])
+			count += 1	
+	print(count)
 
 #Gets hwnd list
-def _get_windows_bytitle(title_text, exact = False):
+def _get_windows_by_title(title_text, exact = False):
 	def _window_callback(hwnd, all_windows):
 		all_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 	windows = []
@@ -13,7 +32,7 @@ def _get_windows_bytitle(title_text, exact = False):
 		return [hwnd for hwnd, title in windows if title_text in title]
 
 def get_window_rect():
-	hwndList = _get_windows_by_title("THUMPER.exe")
+	hwndList = _get_windows_by_title("THUMPER")
 	try:
 		hwnd = hwndList[0]
 	except(IndexError):
